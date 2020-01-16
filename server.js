8fdcd2e601b6cab9275625f50374d37414ea8a8e
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-15 21:26:04 -0500

    added extended to use for express

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({exteded:true}));
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 require("./routes/apiRoutes");
