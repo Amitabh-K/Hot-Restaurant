7c2b3205af37bd64d461b1c05a1d0c4954153cb9
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-15 20:31:42 -0500

    changed index page as default search

--- routes/htmlRout.js
+++ routes/htmlRout.js
@@ -15,8 +15,8 @@ module.exports = function(app) {
     });
 
   // nothing is found move to homepage
-    app.get("/", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/reserve.html"));
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/index.html"));
     });
   };
 
