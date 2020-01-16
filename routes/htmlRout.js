b82a548bf294ee951ad0c32b7107d7c3f87a11cd
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-15 20:00:44 -0500

    comments for html routing and added path

--- routes/htmlRout.js
+++ routes/htmlRout.js
@@ -1,15 +1,20 @@
 const path = require('path');
 
+
 module.exports = function(app) {
+
     
+  // get from tables html
     app.get("/tables", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/tables.html"));
     });
-  
+
+   // get from reserve  html
     app.get("/reserve", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/reserve.html"));
     });
-  
+    
+  // nothing is found move to homepage
     app.get("*", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/reserve.html"));
     });
